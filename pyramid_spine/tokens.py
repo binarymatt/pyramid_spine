37defefaa0417a4d96bdb57abf36518a488e93cc
Matt George <mgeorge@stratasan.com>
2012-12-27 21:21:57 -0600

    adding a token generator for reseting password

---
+++ pyramid_spine/tokens.py
@@ -0,0 +1,46 @@
+import hashlib
+import hmac
+import time
+from pyramid.util import strings_differ
+
+from pyramid_spine.utils import int_to_base62, base62_to_int
+
+class TokenGenerator(object):
+    """
+    general algorithm was borrowed from djangos password reset token generator
+    """
+    def make_token(self, user, key=None):
+        #timestamp is number to seconds since epoch
+        ts = int(time.time())
+        return self._make_token(user, ts, key)
+
+    def _make_token(self, user, ts, key=None):
+        ts_b62 = int_to_base62(ts)
+        if key is None:
+            key = 'pyramid_spint.tokens.TokenGenerator'
+        value = (unicode(user.id) + user.password + unicode(ts))
+        hash = hmac.new(key, value, digestmod=hashlib.sha1).hexdigest()
+        return '%s-%s' % (ts_b62, hash)
+
+    def check_token(self, user, token, limit=None):
+        try:
+            ts_b62, hash = token.split("-")
+        except ValueError:
+            return False
+
+        try:
+            ts = base62_to_int(ts_b62)
+        except ValueError:
+            return False
+
+        if not strings_differ(self.make_token(user, ts), token):
+            return False
+        if limit:
+            now = int(time.time())
+            if ts + int(limit) > now:
+                return False
+
+        return True
+
+
+
--- pyramid_spine/utils.py
+++ pyramid_spine/utils.py
@@ -6,3 +6,27 @@ def utcnow():
     Returns an aware or naive datetime.datetime, depending on settings.USE_TZ.
     """
     return datetime.utcnow().replace(tzinfo=utc)
+
+BASE2 = "01"
+BASE10 = "0123456789"
+BASE16 = "0123456789ABCDEF"
+BASE62 = "AzByCxDwEvFuGtHsIrJqKpLoMnNmOlPkQjRiShTgUfVeWdXcYbZa0123456789"
+def base62_to_int(number):
+    return int(base_convert(number, BASE62, BASE10))
+
+def int_to_base62(number):
+    return base_convert(number, BASE10, BASE62)
+
+def base_convert(number, fromradix, toradix):
+    # based on http://code.activestate.com/recipes/111286/
+    x = long(0)
+    for digit in str(number):
+        x = x * len(fromradix) + fromradix.index(digit)
+
+    res = ""
+    while x > 0:
+        d = x % len(toradix)
+        res = toradix[d] + res
+        x /= len(toradix)
+
+    return res
